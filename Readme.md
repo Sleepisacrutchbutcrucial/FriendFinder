316309c28a2028c6db63854780be88178284b8d0
Justin Fredricksen <fredricksenjustin@gmail.com>
2019-02-13 07:00:47 -0700

    first commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+Undable to render on host via terminal.
+
+Line 8 on server.js bodyparser is not defined which I am unable to identify why.
+
+attatched in images is a screenshot of such event.
---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+var friends = [
+    {
+        name: "Natalie Portman",
+        photo: "http://www.gstatic.com/tv/thumb/persons/71364/71364_v9_ba.jpg",
+        scores: [5, 2, 3, 5, 5, 5, 3, 3, 5, 5]
+    },
+    {
+        name: "Robert Downey Jr.",
+        photo: "http://www.gstatic.com/tv/thumb/persons/67369/67369_v9_bb.jpg",
+        scores: [4, 3, 4, 5, 3, 4, 3, 4, 5, 4]
+    },
+    {
+        name: "Tom Hamks",
+        photo: "http://www.gstatic.com/tv/thumb/persons/62982/62982_v9_bb.jpg",
+        scores: [4, 3, 2, 5, 3, 4, 3, 5, 5, 5]
+    },
+    {
+        name: "Ryan Reynolds",
+        photo: "http://www.gstatic.com/tv/thumb/persons/57282/57282_v9_bb.jpg",
+        scores: [3, 4, 3, 5, 3, 4, 2, 5, 5, 3]
+    },
+    {
+        name: "Barack Obama",
+        photo: "http://www.gstatic.com/tv/thumb/persons/514222/514222_v9_bb.jpg",
+        scores: [5, 3, 3, 5, 4, 5, 5, 5, 5, 4]
+    },
+    {
+        name: "Sandra Bullock",
+        photo: "http://www.gstatic.com/tv/thumb/persons/57807/57807_v9_bb.jpg",
+        scores: [4, 3, 3, 4, 4, 4, 3, 3, 5, 4]
+    },
+    {
+        name: "Donald Trump",
+        photo: "http://www.gstatic.com/tv/thumb/persons/1805/1805_v9_bb.jpg",
+        scores: [1, 1, 5, 1, 1, 1, 5, 1, 1, 1]
+    },
+    {
+        name: "Oprah Winfrey",
+        photo: "http://www.gstatic.com/tv/thumb/persons/49222/49222_v9_ba.jpg",
+        scores: [4, 3, 4, 5, 4, 4, 3, 3, 5, 4]
+    },
+    {
+        name: "Dwayne Johnsom",
+        photo: "http://www.gstatic.com/tv/thumb/persons/235135/235135_v9_ba.jpg",
+        scores: [5, 5, 3, 4, 4, 3, 4, 5, 5, 3]
+    },
+    {
+        name: "Jennifer Lawrence",
+        photo: "http://www.gstatic.com/tv/thumb/persons/389416/389416_v9_bc.jpg",
+        scores: [3, 4, 3, 5, 5, 5, 3, 4, 5, 3]
+    },
+];
+
+module.exports = friends;
